950aa553668b1e4583731ea9f10cb0c8ba7399df
Austin Horstman <khaneliman12@gmail.com>
2026-02-15 12:29:40 -0600

    feat(queries): add csharp
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

---
+++ queries/tuck/c_sharp.scm
@@ -0,0 +1,10 @@
+; Method bodies
+(method_declaration
+  body: (block) @fold)
+
+; Constructor and destructor bodies
+(constructor_declaration
+  body: (block) @fold)
+
+(destructor_declaration
+  body: (block) @fold)
